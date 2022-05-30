234f18971fa81536ea205f0eb27b3420b6db64d1
Zach Howes <zhowes14@icloud.com>
2022-05-29 21:35:38 -0400

    Initial Commit

---
+++ Bobs_Burgers/Bobs_BurgersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bobs_BurgersApp.swift
+//  Bobs_Burgers
+//
+//  Created by Zach Howes on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Bobs_BurgersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bobs_Burgers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bobs_Burgers
+//
+//  Created by Zach Howes on 5/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
