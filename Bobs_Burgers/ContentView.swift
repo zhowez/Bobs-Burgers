a046ecf68b45f2f8332563ffac79881a315a5a4e
Zach Howes <zhowes14@icloud.com>
2022-05-30 00:42:07 -0400

    gets character info and displays picture with name, gender

--- Bobs_Burgers/ContentView.swift
+++ Bobs_Burgers/ContentView.swift
@@ -7,11 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+struct Response: Codable {
+    var result: Result
+}
+
+struct Result: Codable {
+    var id: Int
+    var name: String
+    var image: String
+    var gender:String
+    var hairColor:String
+    var occupation:String
+    var firstEpisode:String
+    var voicedBy:String
+    var url:String
+    var wikiUrl:String
+    
+    init() {
+        id = -1
+        name = " "
+        image = " "
+        gender = " "
+        hairColor = " "
+        occupation = " "
+        firstEpisode = " "
+        voicedBy = " "
+        url = " "
+        wikiUrl = " "
     }
+  
+}
+
+struct ContentView: View {
+    @State private var result = Result()
+   
+    var body: some View {
+        VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: result.image))
+                .frame(width: 100, height: 100)
+            Text(result.name).font(.headline)
+            Text(result.gender)
+                    }
+        .task {
+           await loadData()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://bobsburgers-api.herokuapp.com/characters/127") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession(configuration: .ephemeral).data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(Result.self, from: data) {
+                result = decodedResponse
+            }
+            
+            
+        } catch {
+            print("Invalid data")
+        }
+        
+      print(result)
+       
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
