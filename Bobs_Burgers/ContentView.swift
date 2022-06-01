aa96b81cd94404f8d619d3b29553701fdc886381
Zach Howes <zhowes14@icloud.com>
2022-05-31 23:24:21 -0400

    added all information and a button to get random charcater

--- Bobs_Burgers/ContentView.swift
+++ Bobs_Burgers/ContentView.swift
@@ -36,34 +36,60 @@ struct Result: Codable {
         url = " "
         wikiUrl = " "
     }
-  
+    
 }
 
 struct ContentView: View {
     @State private var result = Result()
-   
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            AsyncImage(url: URL(string: result.image))
-                .frame(width: 100, height: 100)
-            Text(result.name).font(.headline)
-            Text(result.gender)
-                    }
+        VStack(alignment: .center) {
+            Spacer()
+            AsyncImage(url: URL(string: result.image),
+                       content: { image in image.image?.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(minWidth:100,maxWidth: 100, minHeight:175, maxHeight: 175)
+            })
+            
+            VStack(alignment: .leading){
+                Text(result.name).bold().underline().font(.title)
+                Spacer().frame(width: 0, height: 5)
+                Text("Character Info:").font(.title2).bold()
+                Text(result.hairColor)
+                Text(result.gender)
+                Text(result.occupation)
+                Spacer().frame(width: 0, height: 5)
+                Text("Actor Info:").font(.title2).bold()
+                Text(result.firstEpisode)
+                Text(result.voicedBy)
+                
+            }
+            
+            Button(action: {
+                            Task {
+                await loadData()
+            }
+                        }, label: {
+                            Text("Click Me")
+                        })
+            Spacer().frame(width: 0, height: 20)
+        }
         .task {
-           await loadData()
+            await loadData()
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
         
     }
     
     func loadData() async {
-        guard let url = URL(string: "https://bobsburgers-api.herokuapp.com/characters/127") else {
+        let randoInt = Int(arc4random_uniform(502) + 1)
+        guard let url = URL(string: "https://bobsburgers-api.herokuapp.com/characters/\(randoInt)") else {
             print("Invalid URL")
             return
         }
         
         do {
             let (data, _) = try await URLSession(configuration: .ephemeral).data(from: url)
-
+            
             if let decodedResponse = try? JSONDecoder().decode(Result.self, from: data) {
                 result = decodedResponse
             }
@@ -73,8 +99,8 @@ struct ContentView: View {
             print("Invalid data")
         }
         
-      print(result)
-       
+        print(result)
+        
     }
     
     
